62595f2884a22385daade6368b532d9614c13c71
Susan Lunn <slgems@gmail.com>
2015-04-04 18:19:13 -0400

    ugh

--- Aphrodite/Aphrodite.hs
+++ Aphrodite/Aphrodite.hs
@@ -68,11 +68,7 @@ mainloop :: IO ()
 mainloop = scotty 3000 $ do
         middleware $ staticPolicy (noDots >-> addBase "static")
         get "/" $ file "static/index.html" 
-<<<<<<< HEAD
-        get "/clinics/:location" $ do
-=======
         post "/clinics" $ do
->>>>>>> parent of e06d8e6... updated script.js
            location <- param "location"             
            clinics <- liftIO $ getNearbyClinics location
            raw clinics
