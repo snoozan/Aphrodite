08e6fe02f870e0445095b7183174835eba0947d0
Madison Flaherty <mef4824@rit.edu>
2015-04-04 23:53:33 -0400

    Added functionality to Aphrodite.hs to allow get requests for routes for individual routes (start,end).

--- Aphrodite/Aphrodite.hs
+++ Aphrodite/Aphrodite.hs
@@ -34,16 +34,23 @@ getLocation loc = "https://maps.googleapis.com/maps/api/geocode/json?"
                 ++ "address=" ++ urlEncode loc
                 ++ "&key" ++ googleApiKey
 
+getGPSCoor :: String -> Maybe [String]
+getGPSCoor reg = matchRegex (mkRegex "([0-9.-]+) ([0-9.-]+)") reg
+
 
 getNearbyClinics :: String -> IO C.ByteString
 getNearbyClinics loc =
-    case matchRegex (mkRegex "([0-9.-]+) ([0-9.-]+)") loc of
+    case getGPSCoor loc of
         Just [slat,slong] -> simpleHttp $ nearbyUrl $ slat ++ "," ++ slong
         Nothing           -> do res <- simpleHttp $ getLocation loc
                                 let geoloc = decodeJSON $ C.unpack res
                                     M.Geolocresult jsn = geoloc
                                     M.Geolocjson (M.Geolocgeometry (M.Geolocloc lat lng)) = head jsn
                                 simpleHttp $ nearbyUrl $ show lat ++ "," ++ show lng
+
+
+
+
 placeUrl :: String -> String
 placeUrl place = "https://maps.googleapis.com/maps/api/place/details/json?"
                 ++ "placeid=" ++ urlEncode place
@@ -61,7 +68,21 @@ getClinicInfo placeid = do result <- simpleHttp $ placeUrl placeid
                            --        } = dets
                            --    days = encodeJSON pers
                            return result
-    
+directionsUrl :: String -> String -> String
+directionsUrl orig dest = "https://maps.googleapis.com/maps/api/directions/json?"
+                        ++ "Directions"
+                        ++ "&key=" ++ googleApiKey
+                        ++ "origin=" ++ urlEncode orig
+                        ++ "&destination=" ++ urlEncode dest
+                        ++ "&mode=transit"
+
+getRouteFromStr :: Maybe [String] -> Maybe [String] -> IO C.ByteString
+getRouteFromStr (Just [lat1,lng1]) (Just [lat2,lng2]) = do 
+                            res <- simpleHttp $ directionsUrl geoStr1 geoStr2
+                            return res
+                                where geoStr1 = lat1 ++ "," ++ lng1
+                                      geoStr2 = lat2 ++ "," ++ lng2
+
 
 -- Main loop
 mainloop :: IO () 
@@ -76,6 +97,13 @@ mainloop = scotty 3000 $ do
            placeid <- param "placeid"
            clinicDetails <- liftIO $ getClinicInfo placeid
            raw clinicDetails
+        get "/getDirections" $ do
+           start <- param "start"
+           end <- param "end"
+           route <- liftIO (getRouteFromStr (getGPSCoor start) (getGPSCoor end))
+           raw route
+
+
 
 -- Entry Point
 main :: IO ()
